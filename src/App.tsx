5265d5311d39ca3ebe53f59c2a016885e540426a
Kostiantyn Burlai <kostiantyn.burlai@hl.co.uk>
2022-05-26 10:04:38 +0200

    add logo and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
+import './App.scss'
+import Button from 'react-bootstrap/Button';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <h1>Yuliya Burlai IT English</h1>
         <h4>Hello World!</h4>
+        <Button>Bootstrap Button</Button>
       </header>
     </div>
   )
